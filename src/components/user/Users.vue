ab850ccf5054a352fd3bf47bd56601d43a20ba4a
GhostKang <805882988@qq.com>
2020-09-24 20:26:46 +0800

    完善用户列表界面

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,15 +1,122 @@
 <template>
   <div>
+    <!--面包屑-->
     <el-breadcrumb separator="/">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
-    <span>用户列表</span>
+    <!--卡片式布局-->
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8"
+          ><el-input placeholder="请输入内容">
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+            ></el-button> </el-input
+        ></el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <!--用户列表table-->
+      <el-table :data="userlist" style="width: 100%" border stripe>
+        <el-table-column type="index"> </el-table-column>
+        <el-table-column prop="username" label="姓名"> </el-table-column>
+        <el-table-column prop="email" label="邮箱"> </el-table-column>
+        <el-table-column prop="mobile" label="电话"> </el-table-column>
+        <el-table-column prop="role_name" label="角色"> </el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.mg_state"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+            >
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180px">
+          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+          <el-button
+            type="danger"
+            icon="el-icon-delete"
+            size="mini"
+          ></el-button>
+          <el-tooltip
+            class="item"
+            effect="dark"
+            content="设置"
+            placement="top"
+            :enterable="false"
+          >
+            <el-button
+              type="warning"
+              icon="el-icon-setting"
+              size="mini"
+            ></el-button>
+          </el-tooltip>
+        </el-table-column>
+      </el-table>
+      <!--分页-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="userParams.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="userParams.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
+    </el-card>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      userlist: [],
+      //查询参数
+      userParams: {
+        query: "",
+        pagenum: 1,
+        pagesize: 2,
+      },
+      //总页数
+      total: 0,
+    };
+  },
+
+  created() {
+    this.getUserList();
+  },
+
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$http.get("users", {
+        params: this.userParams,
+      });
+      console.log(res);
+      if (res.meta.status != 200) {
+        return this.$message.error(res.meta.msg);
+      }
+      this.userlist = res.data.users;
+      this.total = res.data.total;
+    },
+    //pageSize 改变时会触发
+    handleSizeChange(newPagesize) {
+      this.userParams.pagesize = newPagesize;
+      this.getUserList();
+    },
+    //currentPage 改变时会触发
+    handleCurrentChange(newPagenum) {
+      this.userParams.pagenum = newPagenum;
+      this.getUserList();
+    },
+  },
+};
 </script>
 <style lang="less" scoped>
 </style>
