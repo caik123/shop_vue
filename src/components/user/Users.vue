e3180d07a88185d583e5ac1a7cbc0e6fedc9e4da
GhostKang <805882988@qq.com>
2020-09-25 11:20:07 +0800

    完善用户列表

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -10,14 +10,22 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8"
-          ><el-input placeholder="请输入内容">
+          ><el-input
+            v-model="userParams.query"
+            placeholder="请输入内容"
+            clearable
+            @clear="getUserList()"
+          >
             <el-button
               slot="append"
               icon="el-icon-search"
+              @click="getUserList()"
             ></el-button> </el-input
         ></el-col>
         <el-col :span="4">
-          <el-button type="primary">添加用户</el-button>
+          <el-button type="primary" @click="addUserDialogVisible = true"
+            >添加用户</el-button
+          >
         </el-col>
       </el-row>
       <!--用户列表table-->
@@ -31,8 +39,7 @@
           <template slot-scope="scope">
             <el-switch
               v-model="scope.row.mg_state"
-              active-color="#13ce66"
-              inactive-color="#ff4949"
+              @change="onUserStateChange(scope.row)"
             >
             </el-switch>
           </template>
@@ -71,11 +78,58 @@
       >
       </el-pagination>
     </el-card>
+    <!--添加用户对话框-->
+    <el-dialog
+      title="添加用户"
+      :visible.sync="addUserDialogVisible"
+      width="50%"
+      @close="dialogClose"
+    >
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="70px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addUserDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser()">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
 export default {
   data() {
+    //邮箱验证规则
+    var checkEmail = (rule, value, cb) => {
+      const regEmail = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+      if (regEmail.test(value)) {
+        return cb();
+      }
+      cb(new Error("请输入合法的邮箱"));
+    };
+    var checkMobile = (rule, value, cb) => {
+      const regMobile = /^1[3|4|5|7|8][0-9]{9}$/;
+      if (regMobile.test(value)) {
+        return cb();
+      }
+      cb(new Error("请输入合法的手机号"));
+    };
+
     return {
       userlist: [],
       //查询参数
@@ -86,6 +140,39 @@ export default {
       },
       //总页数
       total: 0,
+      //增加用户对话框显示和隐藏
+      addUserDialogVisible: false,
+      //添加用户表单
+      addForm: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: "",
+      },
+      //添加用户表单验证规则
+      addFormRules: {
+        username: [
+          { required: true, message: "请输入用户名称", trigger: "blur" },
+          { min: 3, max: 6, message: "长度在 3 到 6 个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+        email: [
+          { required: true, message: "请输入邮箱", trigger: "blur" },
+          { validator: checkEmail, trigger: "blur" },
+        ],
+        mobile: [
+          { required: true, message: "请输入手机号", trigger: "blur" },
+          { validator: checkMobile, trigger: "blur" },
+        ],
+      },
     };
   },
 
@@ -114,6 +201,37 @@ export default {
     handleCurrentChange(newPagenum) {
       this.userParams.pagenum = newPagenum;
       this.getUserList();
+    },
+    //修改用户状态
+    async onUserStateChange(userInfo) {
+      console.log(userInfo);
+      const { data: res } = await this.$http.put(
+        `users/${userInfo.id}/state/${userInfo.mg_state}`
+      );
+      console.log(res);
+      if (res.meta.status != 200) {
+        userInfo.mg_state = !userInfo.mg_state;
+        return this.$message.error("修改用户状态失败");
+      }
+      return this.$message.success("修改用户状态成功");
+    },
+    //关闭对话框事件
+    dialogClose() {
+      this.$refs.addFormRef.resetFields();
+    },
+    //点击对话框按钮，添加用户
+    addUser() {
+      this.$refs.addFormRef.validate(async (valid) => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post("users", this.addForm);
+        console.log(res);
+        if (res.meta.status != 201) {
+          return this.$message.error("添加用户失败");
+        }
+        this.$message.success("添加用户成功");
+        this.getUserList();
+        this.addUserDialogVisible = false;
+      });
     },
   },
 };
