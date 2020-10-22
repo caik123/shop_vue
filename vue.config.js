b5d5625b0bc1167e9d6ec16c623b07663e58d910
GhostKang <805882988@qq.com>
2020-10-22 17:13:52 +0800

    删除eslint

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   chainWebpack: config => {
-    //生产模式
+    // 生产模式
     config.when(process.env.NODE_ENV === 'production', config => {
       config.entry('app').clear().add('./src/main-prod.js')
-      config.plugin('html').tap(args=>{
+      config.plugin('html').tap(args => {
         args[0].isProd = true
         return args
       })
     })
-    //开发模式
+    // 开发模式
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.js')
-      config.plugin('html').tap(args=>{
+      config.plugin('html').tap(args => {
         args[0].isProd = false
         return args
       })
